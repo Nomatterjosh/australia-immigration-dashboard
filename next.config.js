e636e1698a0f5f025c1e17b04d1a207d062bbaef
Nomatterjosh <167510882+Nomatterjosh@users.noreply.github.com>
2026-03-26 10:57:21 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
